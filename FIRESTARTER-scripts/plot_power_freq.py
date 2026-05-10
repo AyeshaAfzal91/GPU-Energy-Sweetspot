175eee267e3d69a99f89a762bf60f2c9c63ffa14
Michael Panzlaff <michael.panzlaff@fau.de>
2026-05-11 00:21:49 +0200

    Update Firestarter plots

--- FIRESTARTER-scripts/plot_power_freq.py
+++ FIRESTARTER-scripts/plot_power_freq.py
@@ -6,6 +6,12 @@ import os
 import re
 import sys
 
+class Result:
+    def __init__(self, powerdraw, duration, gflops):
+        self.powerdraw = powerdraw
+        self.duration = duration
+        self.gflops = gflops
+
 def scan_results():
     # Iterate over all directories and search those, which match pattern "FIRESTARTER-*-{frequency,powercap}"
     for d in os.scandir(results_dir):
@@ -37,9 +43,12 @@ def scan_subresults(path, arch, is_freq):
         if not d_match:
             continue
 
-        scan_run(d.path, arch, is_freq, int(d_match[1]))
+        csv_path = scan_nvidiasmi_csv(d.path)
+        log_path = scan_firestarter_log(d.path)
 
-def scan_run(path, arch, is_freq, value):
+        parse_run(csv_path, log_path, arch, is_freq, int(d_match[1]))
+
+def scan_nvidiasmi_csv(path, arch, is_freq, value):
     # Iterate over all files, which looks like *.csv
     # Take the first one and parse that one
     for f in os.scandir(path):
@@ -48,11 +57,23 @@ def scan_run(path, arch, is_freq, value):
         f_match = re.fullmatch("^.*\\.csv$", os.path.basename(f.path))
         if not f_match:
             continue
-        parse_run(f.path, arch, is_freq, value)
-        return
+        return f.path
     raise Exception("Unable to find csv file in directory: {}".format(path))
 
-def parse_run(csv_path, arch, is_freq, value):
+def scan_firestarter_log(path):
+    # Iterate over all files, which looks like *.fsout
+    # Take the first one and parse that one
+    for f in os.scandir(path):
+        if not f.is_file():
+            continue
+        f_match = re.fullmatch("^.*\\.fsout$", os.path.basename(f.path))
+        if not f_match:
+            continue
+        return f.path
+    raise Exception("Unable to find csv file in directory: {}".format(path))
+
+def parse_run(csv_path, log_path, arch, is_freq, value):
+    # Parse nvidia-smi csv
     power_sum = 0.0
     power_iters = 0
     warmup_iters = 20
@@ -79,12 +100,19 @@ def parse_run(csv_path, arch, is_freq, value):
     if power_iters == 0:
         raise Exception("Not enough results in csv: {}", csv_path)
 
+    # TODO fix this
     if is_freq:
         frequency_results[arch][value] = power_sum / power_iters
     else:
         powercap_results[arch][value] = power_sum / power_iters
 
-def create_plots():
+    # TODO parse GFLOPS from firestarter log:
+    with open(log_path, "r") as log_file:
+        for line in log_file:
+            if 
+    # ^.*: ((?:0|[1-9]\d*)(?:\.\d+)?) GFLOPS.*$
+
+def create_power_plots():
     results = [
         {
             "name": "powercap",
@@ -101,21 +129,31 @@ def create_plots():
     ]
 
     for result in results:
+        plt.figure(figsize=(9, 6))
+        plt.xlabel(result["xlabel"])
+        plt.ylabel("Average GPU power draw (W)")
+        plt.title("Avg GPU power draw vs {}".format(result["vs"]))
+        plt.legend(fontsize=8, ncol=2)
+        plt.grid(True)
+
         for arch_name, arch_dict in result["result"].items():
-            plt.figure(figsize=(9, 6))
             keys = sorted(arch_dict)
             values = [arch_dict[k] for k in keys]
-            plt.plot(keys, values, marker="o", linestyle="-", linewidth=1, markersize=3, label="")
-            plt.xlabel(result["xlabel"])
-            plt.ylabel("Average GPU power draw (W)")
-            plt.title("Avg GPU power draw vs {} — {}".format(result["vs"], arch_name))
-            plt.legend(fontsize=8, ncol=2)
-            plt.grid(True)
-            plt.ylim(ymin=0)
+            plt.plot(keys, values, marker="o", linestyle="-", linewidth=1, markersize=3, label=arch_name)
 
-            figure_path = os.path.join(os.path.join(results_dir, "FIRESTARTER-{}-{}".format(arch_name, result["name"])), "plot.svg")
-            plt.savefig(figure_path, dpi=300, bbox_inches="tight")
-            plt.close()
+        plt.ylim(ymin=0)
+        plt.xlim(xmin=0)
+        plt.legend()
+
+        figure_dir = os.path.join(results_dir, "FIRESTARTER-plots")
+        figure_path = os.path.join(figure_dir, "{}.svg".format(result["name"]))
+        os.makedirs(figure_dir, exist_ok=True)
+        plt.savefig(figure_path, dpi=300, bbox_inches="tight")
+        plt.close()
+
+def create_efficiency_plots():
+    for result in results:
+        pass
 
 # Get the parent directory of this script
 results_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -125,4 +163,4 @@ powercap_results = {}
 frequency_results = {}
 
 scan_results()
-create_plots()
+create_power_plots()
